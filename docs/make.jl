c5617ea235f1f17d482e10ef84cb3d5e9790cb11
martinsw01 <martin.s.winther@gmail.com>
2024-09-05 00:05:44 +0200

    Attempting to deploy to gh-pages

--- docs/make.jl
+++ docs/make.jl
@@ -7,10 +7,15 @@ generated_dir = TempJmd.weave_all_files("docs/src")
 
 makedocs(
     sitename="Homemade Conslaws";
-    format = Documenter.HTML(; mathengine=MathEngone.create_mathengine()),
+    format = Documenter.HTML(; mathengine=MathEngine.create_mathengine()),
     pages = [
         "Conservation Laws" => "index.md",
+        "Theory" => [
+            "Linear transport equations" => "theory/linear_transport_eqs.md",
+        ]
     ]
 )
+
+deploydocs(repo="github.com/martinsw01/homemade-conslaws.jl.git")
 
 rm(generated_dir; recursive=true)
